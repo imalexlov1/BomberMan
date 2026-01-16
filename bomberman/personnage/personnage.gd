e5b57a59b4138d6672d2ef88a89c11323ee835f3
imalexlov1 <alex.lovin.pro@gmail.com>
2026-01-16 11:48:55 +0100

    ajout deplacement personnage

---
+++ bomberman/personnage/personnage.gd
@@ -0,0 +1,26 @@
+extends CharacterBody3D
+
+@export var speed = 5.0
+@export var gravity = 9.8
+@export var rotation_speed = 10.0
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	var input_dir = Input.get_vector("gauche", "droite", "avancer", "reculer")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		
+		var target_angle = atan2(direction.x, direction.z)
+		
+		rotation.y = lerp_angle(rotation.y, target_angle, rotation_speed * delta)
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
