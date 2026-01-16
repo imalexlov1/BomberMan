fef88bbcea18bb806000ec541d23efa85a99f2e0
Alex LOVIN <144910136+imalexlov1@users.noreply.github.com>
2026-01-16 12:29:12 +0100

    Refactor direction calculation for character movement

--- bomberman/personnage/personnage.gd
+++ bomberman/personnage/personnage.gd
@@ -9,14 +9,15 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	var input_dir = Input.get_vector("gauche", "droite", "avancer", "reculer")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+
+	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
 	
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
-		
+
 		var target_angle = atan2(direction.x, direction.z)
-		
 		rotation.y = lerp_angle(rotation.y, target_angle, rotation_speed * delta)
 		
 	else:
