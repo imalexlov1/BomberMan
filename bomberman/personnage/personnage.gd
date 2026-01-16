0079dc37f3112edd66b11fc074335f47cd13621a
imalexlov1 <alex.lovin.pro@gmail.com>
2026-01-16 18:43:38 +0100

    feat: add bomb

--- bomberman/personnage/personnage.gd
+++ bomberman/personnage/personnage.gd
@@ -4,24 +4,32 @@ extends CharacterBody3D
 @export var gravity = 9.8
 @export var rotation_speed = 10.0
 
+@export var bomb_scene : PackedScene 
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	var input_dir = Input.get_vector("gauche", "droite", "avancer", "reculer")
-	
+	if Input.is_action_just_pressed("poser_bombe"):
+		spawn_bomb()
 
+	var input_dir = Input.get_vector("gauche", "droite", "avancer", "reculer")
 	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
 	
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
-
 		var target_angle = atan2(direction.x, direction.z)
 		rotation.y = lerp_angle(rotation.y, target_angle, rotation_speed * delta)
-		
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
+
+func spawn_bomb():
+	if bomb_scene: 
+		print("bomb !")
+		var new_bomb = bomb_scene.instantiate()
+		get_parent().add_child(new_bomb)
+		new_bomb.global_position = global_position
