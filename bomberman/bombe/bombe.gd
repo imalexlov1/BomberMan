4e41afae72837c9b281d22070930536b3cf3dafc
alexian-a <alexian.arg11@gmail.com>
2026-01-20 15:51:46 +0100

    Modification de la bombe

--- bomberman/bombe/bombe.gd
+++ bomberman/bombe/bombe.gd
@@ -1,16 +1,22 @@
 extends StaticBody3D
 
+@onready var timer = $Timer
+@onready var anim_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	anim_player.play("blink")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	var time_left = timer.time_left
+	var time_elapsed = 3.0 - time_left
+	anim_player.speed_scale = 1.0 + (time_elapsed * 2.0)
 
 func _on_timer_timeout() -> void:
-	print("BOUM !") 
-	queue_free()
+	print("BOUM !")
+	get_parent().queue_free()
+
+	
+	
